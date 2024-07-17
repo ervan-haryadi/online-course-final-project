df4358c478cedf2aee49b8c167114e4e1cfb2b82
Ervan <ervan.a.haryadi@gmail.com>
2024-07-17 11:35:48 +0700

    register model to admin

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -104,7 +104,7 @@ class Question(models.Model):
     def __str__(self):
         return "Question: " + self.content
     
-    def is_get_score(self):
+    def is_get_score(self, selected_ids):
         all_answers = self.choice_set.filter(is_correct=True).count()
         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
         if all_answers == selected_correct:
